0843b145a1ad99bbe094b2815ad246e9c3d112a8
ZacharyJAdams <zadams.vegas@gmail.com>
2024-06-01 16:48:52 -0700

    Added all pages for the portfolio and added theme.js

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import Layout from './Layout';
+import Home from './scenes/Home';
+import About from './scenes/About';
+import Mission from './scenes/Mission';
+import Projects from './scenes/Projects';
+import Contact from './scenes/Contact';
+import Missing from './scenes/Missing'
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Routes>
+              <Route path="/" element={<Layout />}> 
+
+                {/* public routes */}
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="mission" element={<Mission />} />
+                <Route path="projects" element={<Projects />} />
+                <Route path="contact" element={<Contact />} />
+
+                {/* catch all */}
+                 <Route path="*" element={<Missing />} />
+
+              </Route>
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
