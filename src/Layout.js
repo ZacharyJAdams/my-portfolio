2baa4ed957742d48b3180990ceb49aa6cc2f16dc
ZacharyJAdams <zadams.vegas@gmail.com>
2024-06-03 21:11:06 -0700

    set layout, added favicon.io, and changed colors in theme

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,47 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation, Link } from 'react-router-dom';
 
 const Layout = () => {
+
+    const location = useLocation();
+
+    const renderNavbar = () => {
+
+        if (location.pathname === '/projects' || location.pathname === '/Contact') {
+          return (
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+              </ul>
+            </nav>
+          );
+        } else {
+          return (
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/login">Login</Link></li>
+              </ul>
+            </nav>
+          );
+        }
+
+    };
+
     return (
-        <main className="App">
-            <Outlet />
-        </main>
+        <div>
+            <header>
+                {renderNavbar()}
+            </header>
+            <main className="App">
+                <Outlet />
+            </main>
+            <footer>
+                <p>&copy; 2024 Zachary Adams</p>
+            </footer>
+        </div>
     )
 }
 
