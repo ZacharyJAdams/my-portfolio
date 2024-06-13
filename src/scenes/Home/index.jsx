7320ddc737f5b38f99edf6de9a2008de51c8ba39
ZacharyJAdams <zadams.vegas@gmail.com>
2024-06-13 06:06:44 -0700

    fixing revert issues and recovering navbar

--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Box from '@mui/material/Box'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Box sx={{ mt: 10}}>
+          {[...new Array(50)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box>
+    </div>
   )
 }
 
